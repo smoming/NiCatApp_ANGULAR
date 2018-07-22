82715617c19dfb0310f693ef7099dc1d7a625ce1
smoming <smoming1031@gmail.com>
2018-07-22 15:45:07 +0800

    commodity
    add material style

--- src/app/commodity/commodity.module.ts
+++ src/app/commodity/commodity.module.ts
@@ -6,12 +6,14 @@ import { CommodityRoutingModule } from './commodity-routing.module';
 import { CommodityComponent } from './commodity/commodity.component';
 import { CommodityListComponent } from './commodity-list/commodity-list.component';
 import { CommodityEditComponent } from './commodity-edit/commodity-edit.component';
+import { SharedMaterialModule } from '../shared-material/shared-material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    CommodityRoutingModule
+    CommodityRoutingModule,
+    SharedMaterialModule
   ],
   declarations: [CommodityComponent, CommodityListComponent, CommodityEditComponent]
 })
