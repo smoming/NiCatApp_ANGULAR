1ed19bdb60d03d7a0742792e1a2bcf35bbdab3ee
smoming <smoming1031@gmail.com>
2018-07-22 16:06:16 +0800

    trading
    add material style

--- src/app/trading/trading.module.ts
+++ src/app/trading/trading.module.ts
@@ -7,12 +7,14 @@ import { TradingComponent } from './trading/trading.component';
 import { TradingEditComponent } from './trading-edit/trading-edit.component';
 import { TradingListComponent } from './trading-list/trading-list.component';
 import { TradingQueryComponent } from './trading-query/trading-query.component';
+import { SharedMaterialModule } from '../shared-material/shared-material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    TradingRoutingModule
+    TradingRoutingModule,
+    SharedMaterialModule
   ],
   declarations: [TradingComponent, TradingEditComponent, TradingListComponent, TradingQueryComponent]
 })
