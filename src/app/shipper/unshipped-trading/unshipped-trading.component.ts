e3f38e735d586716e30d7b2976c016e605b70b53
smoming <smoming1031@gmail.com>
2018-08-19 14:04:07 +0800

    shipper
    add mat table

--- src/app/shipper/unshipped-trading/unshipped-trading.component.ts
+++ src/app/shipper/unshipped-trading/unshipped-trading.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { Trading } from '../../trading/trading';
+import { Commodity } from '../../commodity/commodity';
+import { CommodityService } from '../../commodity/commodity.service';
+import { MatTableDataSource, MatCheckboxChange } from '@angular/material';
 
 @Component({
   selector: 'app-unshipped-trading',
@@ -16,15 +19,23 @@ export class UnshippedTradingComponent implements OnInit, OnChanges {
   @Output()
   cancelpayEvent = new EventEmitter<void>();
 
+  commodityList: Commodity[];
+  matTableDS = new MatTableDataSource<any>();
   paylist: string[] = [];
 
-  constructor() { }
+  constructor(private csvc: CommodityService) { }
 
   ngOnInit() {
+    this.csvc.look().subscribe(s => {
+      this.commodityList = s;
+    });
   }
 
   ngOnChanges() {
     // console.log(this.datalist);
+    if (this.datalist) {
+      this.matTableDS.data = this.datalist;
+    }
   }
 
   doPay() {
@@ -36,18 +47,45 @@ export class UnshippedTradingComponent implements OnInit, OnChanges {
     this.cancelpayEvent.emit();
   }
 
-  docheck(item: Event) {
-    const check = (<HTMLInputElement>item.target);
-    const transno = check.getAttribute('data-transno');
-    // console.log(check.checked);
-    // console.log(check.getAttribute('data-transno'));
-    if (check.checked) {
-      this.paylist.push(transno);
+  // docheck(item: Event) {
+  //   const check = (<HTMLInputElement>item.target);
+  //   const transno = check.getAttribute('data-transno');
+  //   // console.log(check.checked);
+  //   // console.log(check.getAttribute('data-transno'));
+  //   if (check.checked) {
+  //     this.paylist.push(transno);
+  //   } else {
+  //     this.paylist = this.paylist.filter(v => {
+  //       return v !== transno;
+  //     });
+  //   }
+  // }
+
+  docheck(event: MatCheckboxChange, row: Trading) {
+    // console.log(event);
+    // console.log(row);
+    if (event.checked) {
+      this.paylist.push(row.TransNo);
     } else {
       this.paylist = this.paylist.filter(v => {
-        return v !== transno;
+        return v !== row.TransNo;
       });
     }
   }
 
+  findname(id: string) {
+    if (this.commodityList) {
+      const commodity = this.commodityList.find(v => {
+        return v.ID === id;
+      });
+
+      if (commodity !== undefined && commodity !== null) {
+        return commodity.Name;
+      } else {
+        return id;
+      }
+    }
+
+    return id;
+  }
 }
